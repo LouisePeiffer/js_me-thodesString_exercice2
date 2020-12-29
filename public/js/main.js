4839f95511ca757f1950421a74cc4d530b45a26c
Louise <louisepeiffer@hotmail.com>
2020-12-29 15:18:38 +0100

    exo 1 à 6

---
+++ public/js/main.js
@@ -0,0 +1,13 @@
+let prenom = prompt('Ton prénom?')
+//alert (`${prenom}`.charAt(0))
+//alert (`${prenom}`.charAt(prenom.length-1))
+//alert (`${prenom}`.substr(1))
+//alert (`${prenom.substr(0,1)}${prenom.charAt(1).toUpperCase()}${prenom.substr(2)}`)
+// let lettre = prompt('Quelle lettre en majuscule?')
+// alert (prenom.replace(lettre, lettre.toUpperCase()))
+
+// 6
+let nom = prompt('Quel est ton nom?')
+alert (`${nom.charAt(0).toUpperCase()}${nom.substr(1)}`)
+
+// 7
