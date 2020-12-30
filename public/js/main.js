d6ef3b9eb173c4411db1ef66e4634077ce2b6c37
Louise <louisepeiffer@hotmail.com>
2020-12-30 09:39:18 +0100

    correction

--- public/js/main.js
+++ public/js/main.js
@@ -1,13 +1,33 @@
 let prenom = prompt('Ton prénom?')
-//alert (`${prenom}`.charAt(0))
-//alert (`${prenom}`.charAt(prenom.length-1))
-//alert (`${prenom}`.substr(1))
-//alert (`${prenom.substr(0,1)}${prenom.charAt(1).toUpperCase()}${prenom.substr(2)}`)
-// let lettre = prompt('Quelle lettre en majuscule?')
-// alert (prenom.replace(lettre, lettre.toUpperCase()))
+//1 Afficher 1ère lettre
+alert (`${prenom}`.charAt(0))
 
-// 6
-let nom = prompt('Quel est ton nom?')
-alert (`${nom.charAt(0).toUpperCase()}${nom.substr(1)}`)
+//2 Afficher dernière lettre
+alert (`${prenom}`.charAt(prenom.length-1))
+
+//3 Afficher prénom sans 1ère lettre
+alert (`${prenom}`.substr(1))
+
+//4 2ème lettre en maj
+prenom = prenom.toLocaleLowerCase();
+// alert (prenom.charAt(0) + prenom.charAt(1).toLocaleLowerCase() + prenom.slice(2));
+// OU
+// alert (`${prenom.substr(0,1)}${prenom.charAt(1).toUpperCase()}${prenom.substr(2)}`)
+let upper = prenom.charAt(1)
+alert(prenom.replace(upper, upper.toUpperCase()))
+
+// 5 Lettre au choix en maj
+let lettre = prompt('Quelle lettre en majuscule?')
+alert (prenom.replace(lettre, lettre.toUpperCase()))
+
+// 6 Peu importe comment prenom écrit, 1ère lettre maj et reste min
+
+//let nom = prompt('Quel est ton nom?')
+//alert (`${nom.charAt(0).toUpperCase()}${nom.substr(1)}`)
+let firstUpper = prenom.charAt(0)
+alert(prenom.replace(first))
+
 
 // 7
+let nbAl = Math.round(Math.random()*prenom.length);
+alert(prenom.replace(prenom.charAt(nbAl), ""));
